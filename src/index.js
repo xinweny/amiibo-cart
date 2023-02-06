1f08e4b143c6ae30aaef7d0958c32a5af33bf8cb
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-06 15:52:49 -0500

    init components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './styles/index.css';
-import App from './components/App';
+import RouteSwitch from './components/RouteSwitch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouteSwitch />
   </React.StrictMode>,
 );
