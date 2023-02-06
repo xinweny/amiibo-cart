3f892202538c7eea89571b9d34a2a54fb4c47316
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-06 16:14:37 -0500

    set up basic navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 function NavBar() {
   return (
     <div className="nav-bar">
       <h1>Amiibo Cart</h1>
+      <ul className="links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/shop">Shop</Link></li>
+      </ul>
     </div>
   );
 }
