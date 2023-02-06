d3eaa27823f051399a805bd6d1e79447c4923720
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-06 15:58:09 -0500

    add non-routing components

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import NavBar from './NavBar';
 import Homepage from './Homepage';
 import Shop from './Shop';
+import Footer from './Footer';
 
 function RouteSwitch() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<Shop />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
