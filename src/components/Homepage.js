82c084d331319712198b8589bae40549d44d0215
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-09 15:33:18 -0500

    fill homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Homepage() {
   return (
     <div>
-      <p>Home</p>
+      <div>
+        <h1>Your one-stop Amiibo shop!</h1>
+        <p>Rare and unique Amiibos for a great deal.</p>
+      </div>
     </div>
   );
 }
