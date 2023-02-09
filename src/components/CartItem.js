32a844069cae9ceae4e05c6520f0f84212aaae05
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-09 12:04:32 -0500

    show/hide edit quantity button in checkout

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,18 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import InputQuantity from './InputQuantity';
 
 function CartItem({ item, editQuantity, deleteItem }) {
   const [quantity, setQuantity] = useState({ ...item }.quantity);
-
-  useEffect(() => editQuantity(item.id, quantity), [quantity]);
+  const [showEditQuantity, setShowEditQuantity] = useState(false);
 
   return (
     <div className="cart-item">
       <div>
         <img src={item.image} alt={item.name} />
         <p>{item.name} ({item.series})</p>
-        <InputQuantity quantity={quantity} setQuantity={setQuantity} />
+        {(showEditQuantity)
+          ? (
+            <div>
+              <InputQuantity quantity={quantity} setQuantity={setQuantity} />
+              <button
+                type="button"
+                onClick={() => {
+                  editQuantity(item.id, quantity);
+                  setShowEditQuantity(false);
+                }}
+              >
+                Update
+              </button>
+            </div>
+          )
+          : (
+            <div>
+              <p>{quantity}</p>
+              <button type="button" onClick={() => setShowEditQuantity(true)}>
+                <img src="#" alt="Show edit quantity" />
+              </button>
+            </div>
+          )}
         <p>${item.price * item.quantity}</p>
       </div>
       <button type="button" onClick={() => deleteItem(item.id)}>x</button>
