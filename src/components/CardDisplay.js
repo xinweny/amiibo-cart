fb4b5d11e77d69e8dd672d66626149de456b18a2
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-02-15 16:55:51 -0500

    fix bug showing slow render

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -5,7 +5,7 @@ import ProductCard from './ProductCard';
 import '../styles/CardDisplay.css';
 
 function CardDisplay({ searchParams, amiibos }) {
-  const [showAmiibos, setShowAmiibos] = useState([...amiibos]);
+  const [showAmiibos, setShowAmiibos] = useState([]);
 
   const series = searchParams.get('series');
   const query = searchParams.get('query');
@@ -15,10 +15,8 @@ function CardDisplay({ searchParams, amiibos }) {
       setShowAmiibos(
         amiibos.filter((amiibo) => amiibo.name.toLowerCase().includes(query.toLowerCase())),
       );
-    } else if (series) {
-      setShowAmiibos((series === 'All')
-        ? amiibos
-        : amiibos.filter((amiibo) => amiibo.amiiboSeries === series));
+    } else if (series && series !== 'All') {
+      setShowAmiibos(amiibos.filter((amiibo) => amiibo.amiiboSeries === series));
     } else {
       setShowAmiibos([...amiibos]);
     }
@@ -28,7 +26,7 @@ function CardDisplay({ searchParams, amiibos }) {
     <div className="card-display">
       <h2>{series
         ? `${series} (${showAmiibos.length})`.toUpperCase()
-        : `Showing ${showAmiibos.length} result${showAmiibos.length > 1 ? 's' : ''} for '${query}'`}
+        : `Showing ${showAmiibos.length} result${showAmiibos.length === 1 ? '' : 's'} for '${query}'`}
       </h2>
       <div className="product-cards">
         {showAmiibos.map((a) => <ProductCard key={a.id} amiibo={a} />)}
